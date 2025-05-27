ec7814c3cf2f029d45432f5fb123e5e7daf09c7e
Marco Castañeda <marco_pascual410@hotmail.com>
2025-05-26 21:12:06 -0600

    update section 86a8wp5pt

--- src/components/SectionBecomeAnAuthor.tsx
+++ src/components/SectionBecomeAnAuthor.tsx
@@ -19,17 +19,18 @@ const SectionBecomeAnAuthor: FC<SectionBecomeAnAuthorProps> = ({
       data-nc-id="SectionBecomeAnAuthor"
     >
       <div className="flex-shrink-0 mb-16 lg:mb-0 lg:mr-10 lg:w-2/5">
-      <Logo className="w-20 self-center" imgLight={{src:"/logo-light.svg",height:280, width:280}} img={{src:"/logo-dark.svg",height:280, width:280}} />
+        <Logo className="w-20 self-center" imgLight={{src:"/logo-light.svg",height:600, width:600}} img={{src:"/logo-dark.svg",height:400, width:400}} />
         <h2 className="font-semibold text-3xl sm:text-4xl mt-6 sm:mt-11">
-          Why did you choose us?
+          Próximo GScouting Trip: Camping Isla Cerralvo
         </h2>
-        <span className="block mt-6 text-neutral-500 dark:text-neutral-400">
-          Accompanying us, you have a trip full of experiences. With Chisfis,
-          booking accommodation, resort villas, hotels, private houses,
-          apartments... becomes fast, convenient and easy.
+        <span className="block md:text-lg text-neutral-500 dark:text-neutral-400 mt-2">
+          Explora antes que todos un paraíso aún sin guías
+        </span>
+        <span className="block mt-5 text-neutral-500 dark:text-neutral-400">
+          Únete a nuestra expedición exclusiva rumbo a Isla Cerralvo, también conocida como Isla Jacques Cousteau. Te llevamos a acampar en uno de los lugares más prístinos del Mar de Cortés, donde no llegan los tours ni el wifi, pero sí las historias que se quedan para siempre.
         </span>
         <ButtonPrimary className="mt-6 sm:mt-11">
-          Become an author
+          Únete como Scout
         </ButtonPrimary>
       </div>
       <div className="flex-grow">
